35fee7ebe2b148417c3d99df8760ac6b90bb4318
Ryan Tablada <ryan.tablada@gmail.com>
2016-04-07 15:28:42 -0500

    Update no generator for ember-cli-sass

--- bin/starter.sh
+++ bin/starter.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
 npm install --save-dev eslint eslint-config-rtablada sass-lint ember-cli-sass ember-font-awesome
-ember g ember-cli-sass
 ember g ember-font-awesome
 wget https://raw.githubusercontent.com/TIY-TN-FEE-2016-spring/ember-start/master/.eslintrc -O .eslintrc
 wget https://raw.githubusercontent.com/TIY-TN-FEE-2016-spring/ember-start/master/.ember-cli -O .ember-cli
