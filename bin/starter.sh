8e91a91a05a5e61be3e99ac395ebb0df85a2daa8
Ryan Tablada <ryan.tablada@gmail.com>
2016-04-07 12:31:18 -0500

    Update to use npm not ember for installs

--- bin/starter.sh
+++ bin/starter.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
-ember install eslint eslint-config-rtablada sass-lint ember-cli-sass ember-font-awesome
+npm install --save-dev eslint eslint-config-rtablada sass-lint ember-cli-sass ember-font-awesome
+ember g ember-cli-sass
+ember g ember-font-awesome
 wget https://raw.githubusercontent.com/TIY-TN-FEE-2016-spring/ember-start/master/.eslintrc -O .eslintrc
 wget https://raw.githubusercontent.com/TIY-TN-FEE-2016-spring/ember-start/master/.ember-cli -O .ember-cli
 wget https://raw.githubusercontent.com/TIY-TN-FEE-2016-spring/ember-start/master/.sass-lint.yml -O .sass-lint.yml
+mv app/styles/app.css app/styles/app.scss
