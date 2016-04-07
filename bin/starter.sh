a559e61622f0452b31230783f3e5e81abbd51a45
Ryan Tablada <ryan.tablada@gmail.com>
2016-04-07 12:23:18 -0500

    Update implementation

---
+++ bin/starter.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+ember install eslint eslint-config-rtablada sass-lint ember-cli-sass ember-font-awesome
+wget https://raw.githubusercontent.com/TIY-TN-FEE-2016-spring/ember-start/master/.eslintrc -O .eslintrc
+wget https://raw.githubusercontent.com/TIY-TN-FEE-2016-spring/ember-start/master/.ember-cli -O .ember-cli
+wget https://raw.githubusercontent.com/TIY-TN-FEE-2016-spring/ember-start/master/.sass-lint.yml -O .sass-lint.yml
