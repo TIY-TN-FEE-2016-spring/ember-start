7874f6311ae2f98df5331f7dcd5691f88e2ff915
Ryan Tablada <ryan.tablada@gmail.com>
2016-04-20 08:47:28 -0500

    Concurrency!!!

--- bin/starter.sh
+++ bin/starter.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
-npm install --save-dev eslint eslint-config-rtablada sass-lint ember-cli-sass ember-font-awesome
+npm install --save-dev eslint eslint-config-rtablada sass-lint ember-cli-sass ember-font-awesome ember-concurrency ember-truth-helpers
 ember g ember-font-awesome
 wget https://raw.githubusercontent.com/TIY-TN-FEE-2016-spring/ember-start/master/.eslintrc -O .eslintrc
 wget https://raw.githubusercontent.com/TIY-TN-FEE-2016-spring/ember-start/master/.ember-cli -O .ember-cli
 wget https://raw.githubusercontent.com/TIY-TN-FEE-2016-spring/ember-start/master/.sass-lint.yml -O .sass-lint.yml
+wget https://raw.githubusercontent.com/TIY-TN-FEE-2016-spring/ember-start/master/ember-cli-build.js -O ember-cli-build.js
 mv app/styles/app.css app/styles/app.scss
